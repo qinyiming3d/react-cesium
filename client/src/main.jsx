22967d7849519ba6c4d24ad59e08bd5d2cb52cda
覃艺明 <526454676@qq.com>
2025-04-06 04:21:13 +0800

    feat: 引入默认样式

---
+++ client/src/main.jsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './i18n'
+import './baseStyle.css'
+
+createRoot(document.getElementById('root')).render(
+  <App />
+)
