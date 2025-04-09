49b251d82f2cc4e7dfbe979cea5a48663eefed74
覃艺明 <526454676@qq.com>
2025-04-09 21:42:30 +0800

    feat: 配置前端全局目录

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cesium from 'vite-plugin-cesium';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cesium()],
   resolve: {
+    alias: {
+      '@_public': path.resolve(__dirname, './src/_public'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      "@components": path.resolve(__dirname, './src/components'),
+    },
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   server: {
