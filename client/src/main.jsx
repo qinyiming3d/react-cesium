fdf4c390dc678f8d8feca2c792d9970076f88168
覃艺明 <526454676@qq.com>
2025-04-20 19:26:28 +0800

    feat：增加nprogress样式文件

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './i18n'
 import './baseStyle.css'
+import 'nprogress/nprogress.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
